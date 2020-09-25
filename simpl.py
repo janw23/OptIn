72e23b09f08b0749f7b3ecdba88337dd5acc10a6
Jan Wawszczak <janekw23@gmail.com>
2020-09-25 21:35:42 +0200

    Add neural network state predictor to the first AgentEnvironment - it works (sort of). Add OpenAI's gym to environment.yml.

--- simpl.py
+++ simpl.py
@@ -3,8 +3,9 @@ import time
 import random
 
 import torch
+import torch.nn as nn
 
-from optin.utils import clip, clear_terminal
+from optin.utils import clip, lerp, clear_terminal
 
 
 class AgentEnvironment:
@@ -15,13 +16,14 @@ class AgentEnvironment:
 
     def control_agent(self, agent_input: float):
         self.agent_pos += clip(agent_input, -1.0, 1.0)
+        self.agent_pos = clip(self.agent_pos, -1., 1.)
 
     def draw_state(self):
         def _draw_pos(pos: float):
-            pos = pos * 5 + 5  # from (-1, 1) to (0, 10)
+            pos = (pos + 1) / 2  # from (-1, 1) to (0, 1)
+            pos = lerp(pos, 1, 30)
             pos = round(pos)
-            assert 0 <= pos <= 10
-            drawing = ['o'] * 11
+            drawing = ['o'] * 31
             drawing[pos] = 'O'
             return ''.join(drawing)
 
@@ -32,10 +34,49 @@ class AgentEnvironment:
         return (self.agent_pos, self.agent_target_pos)
 
 
+class SimpleStatePredictor(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.linear1 = nn.Linear(2, 5)
+        self.linear2 = nn.Linear(5, 5)
+        self.linear3 = nn.Linear(5, 1)
+
+        self.relu = nn.ReLU()
+
+        self.optim = torch.optim.SGD(self.parameters(), 0.001)
+
+    def forward(self, last_state: torch.Tensor, action: torch.Tensor):
+        x = torch.stack((last_state, action), dim=-1)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        return x
+
+
+    def optimize(
+            self, last_state: torch.Tensor, last_action: torch.Tensor,
+            next_state: torch.Tensor
+    ):
+        self.optim.zero_grad()
+        predicted_state = self(last_state, last_action)
+        loss = torch.abs(next_state - predicted_state).square()
+        loss.backward()
+        self.optim.step()
+
+        print('state predictor loss:', loss.item())
+
+
 class SimpleAgent:
 
     def __init__(self, env: AgentEnvironment):
         self.env = env
+        self.state_predictor = SimpleStatePredictor()
+
+        self.prev_state = torch.zeros(1)
+        self.prev_action = torch.zeros(1)
 
     def make_action(self):
         pos, target_pos = env.get_agent_state()
@@ -43,17 +84,22 @@ class SimpleAgent:
         state = torch.tensor(pos)
         target_state = torch.tensor(target_pos)
 
+        self.state_predictor.optimize(self.prev_state, self.prev_action, state)
+
         action = torch.tensor(0.0, requires_grad=True)
         optim = torch.optim.Adam([action], 0.1)
 
-        for optimizer_step in range(1):
-            predicted_next_state = self.predict_next_state(state, action)
+        for optimizer_step in range(4):
+            predicted_next_state = self.state_predictor(state, action)
 
             loss = torch.abs(target_state - predicted_next_state)
 
             loss.backward()
             optim.step()
             optim.zero_grad()
+
+        self.prev_state = state
+        self.prev_action = action
 
         return action.item()
 
@@ -67,11 +113,12 @@ if __name__ == '__main__':
     env = AgentEnvironment()
     agent = SimpleAgent(env)
 
-    for i in range(100):
+    for i in range(10000):
+        clear_terminal()
+
         action = agent.make_action()
         env.control_agent(action)
 
-        clear_terminal()
         env.draw_state()
         print('action', action)
 
